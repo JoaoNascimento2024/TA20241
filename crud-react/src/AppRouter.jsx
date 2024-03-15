6446aa2ef93d51891fabd16c53d6a720540c2a29
JoaoNascimento2024 <1724026@ifrn.local>
2024-03-15 18:10:36 -0300

    Início do crud de turmas

--- crud-react/src/AppRouter.jsx
+++ crud-react/src/AppRouter.jsx
@@ -1,12 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./paginas/Home";
+import NaoEncontrada from "./paginas/NaoEncontrada";
+import Base from "./paginas/Base";
+import Turma from "./paginas/Turma";
 
 
 function AppRouter() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home/>}/>                
+                <Route path="/" element={<Base/>}>                                    
+                    <Route index element={<Home/>}/> 
+                    <Route path="/turmas" element={<Turma/>}/>               
+                    <Route path="*" element={<NaoEncontrada/>}/>               
+                </Route>
             </Routes>
         </BrowserRouter>
     );
