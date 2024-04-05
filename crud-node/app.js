11d1d68dce7d2ec15bb38bbe5182bc31ac2fa86f
JoaoNascimento2024 <1724026@ifrn.local>
2024-04-05 17:26:36 -0300

    AlunoController

--- crud-node/app.js
+++ crud-node/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const turmaController = require("./controllers/TurmaController");
+const alunoController = require("./controllers/AlunoController");
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/turmas",turmaController);
+app.use("/alunos",alunoController);
 
 app.listen(3000, () => {
     console.log("Servidor conectado");
